3ea6dda43efe51e62ba205b79ac1be08772f62fe
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-07-19 10:55:57 +0600

    create follow serializer and view

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ProfileRetrieveAPIView
+from .views import ProfileRetrieveAPIView, ProfileFollowAPIView
 
 app_name = 'profiles'
 
 urlpatterns = [
     path('profiles/<str:username>', ProfileRetrieveAPIView.as_view()),
+    path('profiles/<str:username>/follow', ProfileFollowAPIView.as_view()),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,9 @@
-from rest_framework import status
+from rest_framework import status, serializers
 from rest_framework.generics import RetrieveAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
+from rest_framework.views import APIView
 
 from .models import Profile
 from .renderers import ProfileJSONRenderer
@@ -29,6 +30,43 @@ class ProfileRetrieveAPIView(RetrieveAPIView):
             # raise ProfileDoesNotExist
             raise NotFound(f'A profile with this {username} does not exist.')
 
-        serializer = self.serializer_class(profile)
-
+        # serializer = self.serializer_class(profile)
+        serializer = self.serializer_class(profile, context={'request': request})
+        # In the changes we just made to the serializer, you may have noticed that
+        # we’re looking for the request object inside the serializer. This change to
+        # ProfileRetrieveAPIView is how we can do that
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+class ProfileFollowAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+    renderer_classes = (ProfileJSONRenderer,)
+    serializer_class = ProfileSerializer
+
+    def delete(self, request, username=None):
+        follower = self.request.user.profile
+        try:
+            followee = Profile.objects.get(user__username=username)
+        except Profile.DoesNotExist:
+            raise NotFound('A profile with this username was not found.')
+
+        follower.unfollow(followee)
+        serializer = self.serializer_class(followee, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, username=None):
+        follower = self.request.user.profile
+        try:
+            followee = Profile.objects.get(user__username=username)
+        except Profile.DoesNotExist:
+            raise NotFound('A profile with this username was not found.')
+        if follower.pk is followee.pk:
+            raise serializers.ValidationError('You can not follow yourself.')
+
+        follower.follow(followee)
+        serializer = self.serializer_class(followee, context={
+            'request': request
+        })
+        
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
